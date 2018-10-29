77aa388edac4757dda1297b37085ee5b3d89968f
biancacharlotin <biancacharlotin@gmail.com>
2018-10-29 20:16:19 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,19 +1,30 @@
 def addition(num1, num2)
+  num1=12 + num2=4 
 end
 
 def subtraction(num1, num2)
+    num1=12 - num2=4 
+
 end
 
 def division(num1, num2)
+    num1=12 / num2=4 
+
 end
 
-def multiplication(num1, num2)
+def multiplication(num1, num2)  
+  num1=12 * num2=4 
+
 end
 
 def modulo(num1, num2)
+    num1=12 % num2=4 
+
 end
 
 def square_root(num)
+  num=72
+  math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
